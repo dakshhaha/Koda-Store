51c5ab25b479be413c0405191410b7d0f5ce5e6e
dakshhaha <offprivatedaksh@gmail.com>
2026-04-14 11:36:54 +0530

    fix(prod): provide secret in authConfig to fix edge middleware MissingSecret error

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,6 +2,7 @@ import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
 export const authConfig = {
+  secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/auth/login",
     newUser: "/auth/signup",
