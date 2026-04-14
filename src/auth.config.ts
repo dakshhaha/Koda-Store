f62f423792ee4fc9b10277168514145e87beb993
dakshhaha <offprivatedaksh@gmail.com>
2026-04-14 11:47:54 +0530

    fix(prod): provide hardcoded fallback secret to bypass vercel MissingSecret crash if env variables are missing

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,7 +2,7 @@ import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
 export const authConfig = {
-  secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
+  secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET || "fallback_secret_for_vercel_edge_runtime_crash_protection",
   pages: {
     signIn: "/auth/login",
     newUser: "/auth/signup",
